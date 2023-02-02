6e623c4c0d2398669842ec4219838f4d72ab34ce
Alek30k <alemank23k@gmail.com>
2023-02-02 11:44:22 -0300

    last commit

--- index.js
+++ index.js
@@ -27,14 +27,20 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
-  
-    const values = [
-      req.body.title,
-      req.body.desc,
-      req.body.price,
-      req.body.cover,
-    ];
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
 
 app.listen(port, () => {
   console.log("Connected to port " + port);
