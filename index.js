0fa4739f9b798160675e58ca8ad8bcea769dd01f
Alek30k <alemank23k@gmail.com>
2023-02-02 11:09:59 -0300

    last commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
-
 const port = 8800;
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "book",
+});
 
 app.listen(port, () => {
   console.log("Connected to port " + port);
