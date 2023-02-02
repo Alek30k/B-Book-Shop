1988e69e97184118de7d3e7a09e98692accd46ab
Alek30k <alemank23k@gmail.com>
2023-02-02 11:12:54 -0300

    last commit

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const db = mysql.createConnection({
   database: "book",
 });
 
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
 app.listen(port, () => {
   console.log("Connected to port " + port);
 });
